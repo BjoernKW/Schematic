9add851ab781d26e1a8112447f4fc56622b9efd8
Björn Wilmsmann <bjoern@bjoernkw.com>
2022-12-15 18:03:49 +0100

    Initial commit.

---
+++ src/main/java/com/bjoernkw/schematic/SchematicApplication.java
@@ -0,0 +1,13 @@
+package com.bjoernkw.schematic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchematicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchematicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bjoernkw/schematic/SchematicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjoernkw.schematic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchematicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
