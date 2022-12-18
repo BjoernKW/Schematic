fe5ee5fd6f98cd3b634e49e6efd671c9ab0c5cb1
Björn Wilmsmann <bjoern@bjoernkw.com>
2022-12-18 02:34:27 +0100

    First prototype

---
+++ src/main/java/com/bjoernkw/schematic/database/IndexController.java
@@ -0,0 +1,15 @@
+package com.bjoernkw.schematic.database;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String redirect() {
+        return "redirect:/tables";
+    }
+}
