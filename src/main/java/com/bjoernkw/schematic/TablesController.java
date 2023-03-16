3d9abd3bfc10c07251f154df4cc94c2992e94c4e
Björn Wilmsmann <bjoern@bjoernkw.com>
2023-03-16 20:44:11 +0100

    Code formatting

--- src/main/java/com/bjoernkw/schematic/TablesController.java
+++ src/main/java/com/bjoernkw/schematic/TablesController.java
@@ -159,35 +159,35 @@ public class TablesController {
         if (driverClassName.equals("class org.postgresql.Driver")) {
             // See https://www.cybertec-postgresql.com/en/er-diagrams-with-sql-and-mermaid/#
             String sqlQuery = """
-            SELECT 'erDiagram' AS mermaid_diagram_line
-            UNION ALL
-            SELECT
-                format(E'\\t%s\s{\\n%s\\n\\t}',\s
-                    c.relname,\s
-                    string_agg(format(E'\\t\\t%s %s',\s
-                        t.typname,\s
-                        a.attname
-                    ), E'\\n'))
-            FROM
-                pg_class c\s
-                JOIN pg_namespace n ON n.oid = c.relnamespace
-                LEFT JOIN pg_attribute a ON c.oid = a.attrelid AND a.attnum > 0 AND NOT a.attisdropped
-                LEFT JOIN pg_type t ON a.atttypid = t.oid
-            WHERE
-                c.relkind IN ('r', 'p')\s
-                AND NOT c.relispartition
-                AND n.nspname !~ '^pg_' AND n.nspname <> 'information_schema'
-            GROUP BY c.relname
-            UNION ALL
-            SELECT
-                format('%s }|..|| %s : %s', c1.relname, c2.relname, c.conname)
-            FROM
-                pg_constraint c
-                JOIN pg_class c1 ON c.conrelid = c1.oid AND c.contype = 'f'
-                JOIN pg_class c2 ON c.confrelid = c2.oid
-            WHERE
-                NOT c1.relispartition AND NOT c2.relispartition;
-        """;
+                SELECT 'erDiagram' AS mermaid_diagram_line
+                UNION ALL
+                SELECT
+                    format(E'\\t%s\s{\\n%s\\n\\t}',\s
+                        c.relname,\s
+                        string_agg(format(E'\\t\\t%s %s',\s
+                            t.typname,\s
+                            a.attname
+                        ), E'\\n'))
+                FROM
+                    pg_class c\s
+                    JOIN pg_namespace n ON n.oid = c.relnamespace
+                    LEFT JOIN pg_attribute a ON c.oid = a.attrelid AND a.attnum > 0 AND NOT a.attisdropped
+                    LEFT JOIN pg_type t ON a.atttypid = t.oid
+                WHERE
+                    c.relkind IN ('r', 'p')\s
+                    AND NOT c.relispartition
+                    AND n.nspname !~ '^pg_' AND n.nspname <> 'information_schema'
+                GROUP BY c.relname
+                UNION ALL
+                SELECT
+                    format('%s }|..|| %s : %s', c1.relname, c2.relname, c.conname)
+                FROM
+                    pg_constraint c
+                    JOIN pg_class c1 ON c.conrelid = c1.oid AND c.contype = 'f'
+                    JOIN pg_class c2 ON c.confrelid = c2.oid
+                WHERE
+                    NOT c1.relispartition AND NOT c2.relispartition;
+            """;
 
             StringBuilder output = new StringBuilder();
             List<Map<String, Object>> queryResultRows = jdbcTemplate.queryForList(sqlQuery);
